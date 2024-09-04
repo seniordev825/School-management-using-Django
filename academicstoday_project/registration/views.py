76430bb26b53c686efd441d089d99cbb4fd91bbb
Bright <134352369+seniordev825@users.noreply.github.com>
2024-09-04 02:38:48 +0000

    Update views.py
    Updated

--- academicstoday_project/registration/views.py
+++ academicstoday_project/registration/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from registration.form import RegisterForm
-
+ 
 
 def register_modal(request):
     form = RegisterForm()
